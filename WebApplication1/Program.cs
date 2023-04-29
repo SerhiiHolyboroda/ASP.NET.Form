9577cb4ab6ce9733c0709261bfb9a8f091cfc74f
Serhii <nosferatugamevampire@gmail.com>
2023-04-29 19:38:08 +0300

    adding files

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+
+    if (context.Request.Path == "/postuser")
+    {
+        var form = context.Request.Form;
+        string name = form["name"];
+        string mail = form["mail"];
+        await context.Response.WriteAsync($"<div><p>I, {name} email: {mail} became ASP .NET developer</p></div>");
+    }
+    else
+    {
+        await context.Response.SendFileAsync("html/index.html");
+    }
+});
 
 app.Run();
